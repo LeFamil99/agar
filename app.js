85c8ba94046f6ad90e0f6b00cf733b1f9791aeae
LeFamil99 <maxgaudreau9@gmail.com>
2021-01-18 17:47:55 -0500

    G

--- app.js
+++ app.js
@@ -15,36 +15,86 @@ app.use(express.static(path.join(__dirname, "public")));
     res.sendFile(__dirname + "/index.html") 
 });;*/
 
-const hasName = false;
+let hasName = false;
 let users = 0;
+let usersNamed = 0;
+let food = []; 
 
 
-io.on("connection", (socket) => {
-  debug("a user connected");
+io.on("connection", (socket) => { 
+  hasName = false;
   users++;
+  debug("a user connected"); 
+  
 
   socket.on("disconnect", () => {
+    if(hasName)
+    usersNamed--;
     users--;
     debug("user disconnected");
     socket.broadcast.emit("disconnected", socket.id)
   });
 
   socket.on("position", pos => {
-    socket.broadcast.emit("position", {pos: pos, user: socket.username, id: socket.id, color: socket.color});
+    socket.emit("food", food); 
+    socket.broadcast.emit("position", {
+      pos: pos, 
+      user: socket.username, 
+      id: socket.id, 
+      color: socket.color
+    });
     //debug(pos);
   });
 
   socket.on("set name", x => {
+    hasName = true;
+    usersNamed++;
     socket.username = x.user;
     socket.color = x.color
     debug(socket.username)
     socket.broadcast.emit("connected", {pos: {
       x: 0,
-      y: 0,
+      y: 0, 
       r: 0
     }, user: socket.username, id: socket.id})
-  })
+  });
+
+  socket.on("eaten", i => {
+    food.splice(i, 1);
+  });
+
+  socket.on("kill", id => {
+    socket.emit("disconnected", id);
+    socket.broadcast.to(id).emit("kill", socket.id);
+    usersNamed--;
+  });
+
+  socket.on("noname", () => {
+    hasName = false
+  });
 });
+
+function generateFood() {
+  if(usersNamed > 0) {
+    food.push({
+      color: Math.floor(Math.random() * 12),
+      x: Math.floor(Math.random() * 1940 + 30),
+      y: Math.floor(Math.random() * 940 + 30),
+    })
+    if(food.length > 800) {
+      food.shift();
+    }
+  } else {
+    food = [];
+  }
+  
+  //debug(usersNamed);
+  // debug(users);
+  setTimeout(generateFood, 30);
+}
+generateFood();
+
+
 
 const port = process.env.PORT || 3000;
 
--- public/main.js
+++ public/main.js
@@ -11,32 +11,44 @@ const ctx = canvas.getContext("2d");
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
+//-------------------- Variables
+
+//Movement
 let direction = {x: 0, y: 0};
 let deadzone = 1;
 let decalRange = 20;
 let decalX = 0;
 let decalY = 0;
 
+//Players
 let x = 200;
 let y = 200;
-let speed = 5;
-let radius = 50;
+let radius = 20;
 let velX = 0;
 let velY = 0;
 let alt = [];
-let arrRight = false, arrLeft = false, arrUp = false, arrDown = false;
+//let arrRight = false, arrLeft = false, arrUp = false, arrDown = false;
 let username;
 let color;
+let score = 20;
+let speed = 50 / (score + 1) + 2.5;
+let dead = false
+let immune;
 
+//Map
 let spacing = 50;
 let mapWidth = 2000;
 let mapHeight = 1000;
-borderThickness = 100000;
+let borderThickness = 100000;
+
+//Food
+let food;
+let foodRadius = 10;
 
 const colors = [
-  '#e21400', '#91580f', '#f8a700', '#f78b00',
-  '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
-  '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
+  '#0066ff', '#00ffff', '#00ff00', '#99ff33',
+  '#ffff00', '#ffcc66', '#ff0000', '#ff0066',
+  '#cc0099', '#9933ff', '#3333ff', '#ccccff'
 ];
 
 /*ctx.beginPath();
@@ -45,103 +57,185 @@ ctx.lineTo(100, 100);
 ctx.stroke();*/
 
 const frame = () => {
-  /*if(arrRight)
-    x += speed;
 
-  if(arrLeft)
-    x -= speed;
 
-  if(arrDown)
-    y += speed;
+  if(!dead){
+    //-------------------- Movement
+    let lastX = x;
+    let lastY = y;
 
-  if(arrUp)
-    y -= speed;*/
-  let lastX = x;
-  let lastY = y;
+    x += direction.x * speed;
+    y += direction.y * speed;
+    x = Math.min(mapWidth - radius, x)
+    x = Math.max(radius, x)
+    y = Math.min(mapHeight - radius, y)
+    y = Math.max(radius, y)
+    
+    decalX += ((x - lastX) / speed * decalRange - decalX) / 20;
+    decalY += ((y - lastY) / speed * decalRange - decalY) / 20;
 
-  x += direction.x * speed;
-  y += direction.y * speed;
-  x = Math.min(mapWidth - radius, x)
-  x = Math.max(radius, x)
-  y = Math.min(mapHeight - radius, y)
-  y = Math.max(radius, y)
-  
-  decalX += ((x - lastX) / speed * decalRange - decalX) / 20;
-  decalY += ((y - lastY) / speed * decalRange - decalY) / 20;
+    /*x += velX;
+    y += velY;*/
 
-  /*x += velX;
-  y += velY;*/
+    //-------------------- Drawing
 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ctx.lineWidth = borderThickness;
-  ctx.beginPath();
-  ctx.moveTo(-x + canvas.width / 2 - borderThickness / 2, -y + canvas.height / 2 - borderThickness / 2);
-  ctx.lineTo(-x + canvas.width / 2 - borderThickness / 2, mapHeight - y + canvas.height / 2 + borderThickness / 2);
-  ctx.lineTo(mapWidth - x + canvas.width / 2 + borderThickness / 2, mapHeight - y + canvas.height / 2 + borderThickness / 2);
-  ctx.lineTo(mapWidth - x + canvas.width / 2 + borderThickness / 2, -y + canvas.height / 2 - borderThickness / 2);
-  ctx.lineTo(-x + canvas.width / 2 - borderThickness / 2, -y + canvas.height / 2 - borderThickness / 2);
-  ctx.stroke();
-
-  ctx.lineWidth = 1;
-  for (let i = -x % spacing; i <= window.innerWidth; i += spacing) {
-    if(i > -x + canvas.width / 2 && i < -x + canvas.width / 2 + mapWidth) {
-      ctx.beginPath();
-      ctx.moveTo(i, Math.max(0, -y + canvas.height / 2 + mapHeight));
-      ctx.lineTo(i, Math.min(window.innerHeight, -y + canvas.height / 2));
-      ctx.stroke();
-    }
-  }
-  for (let i = -y % spacing; i <= window.innerHeight; i += spacing) {
-    if(i > -y + canvas.height / 2 && i < -y + canvas.height / 2 + mapHeight) {
-      ctx.beginPath();
-      ctx.moveTo(Math.max(0, -x + canvas.width / 2), i);
-      ctx.lineTo(Math.min(window.innerWidth, -x + canvas.width / 2 + mapWidth), i);
-      ctx.stroke();
-    }
-  }
-
-  
-
-  for(let i in alt) {
-    //console.log(alt[i]);
+    //Border
+    /*ctx.lineWidth = borderThickness;
     ctx.beginPath();
-    ctx.arc(alt[i].pos.x - (x - canvas.width / 2), alt[i].pos.y - (y - canvas.height / 2), alt[i].pos.r, 0, Math.PI * 2);
-    ctx.fillStyle = colors[alt[i].color];
+    ctx.moveTo(-x + canvas.width / 2 - borderThickness / 2, -y + canvas.height / 2 - borderThickness / 2);
+    ctx.lineTo(-x + canvas.width / 2 - borderThickness / 2, mapHeight - y + canvas.height / 2 + borderThickness / 2);
+    ctx.lineTo(mapWidth - x + canvas.width / 2 + borderThickness / 2, mapHeight - y + canvas.height / 2 + borderThickness / 2);
+    ctx.lineTo(mapWidth - x + canvas.width / 2 + borderThickness / 2, -y + canvas.height / 2 - borderThickness / 2);
+    ctx.lineTo(-x + canvas.width / 2 - borderThickness / 2, -y + canvas.height / 2 - borderThickness / 2);
+    ctx.stroke();*/
+
+    //Grid
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#cccccc";
+    for (let i = -x % spacing; i <= window.innerWidth; i += spacing) {
+      ctx.beginPath();
+      ctx.moveTo(i, 0);
+      ctx.lineTo(i, window.innerHeight);
+      ctx.stroke();
+
+    }
+    for (let i = -y % spacing; i <= window.innerHeight; i += spacing) {
+      ctx.beginPath();
+      ctx.moveTo(0, i);
+      ctx.lineTo(window.innerWidth, i);
+      ctx.stroke();
+    }
+
+
+    /*for (let i = -x % spacing; i <= window.innerWidth; i += spacing) {
+      if(i > -x + canvas.width / 2 && i < -x + canvas.width / 2 + mapWidth) {
+        ctx.beginPath();
+        ctx.moveTo(i, Math.max(0, -y + canvas.height / 2 + mapHeight));
+        ctx.lineTo(i, Math.min(window.innerHeight, -y + canvas.height / 2));
+        ctx.stroke();
+      }
+    }
+    for (let i = -y % spacing; i <= window.innerHeight; i += spacing) {
+      if(i > -y + canvas.height / 2 && i < -y + canvas.height / 2 + mapHeight) {
+        ctx.beginPath();
+        ctx.moveTo(Math.max(0, -x + canvas.width / 2), i);
+        ctx.lineTo(Math.min(window.innerWidth, -x + canvas.width / 2 + mapWidth), i);
+        ctx.stroke();
+      }
+    }*/
+
+    //Food
+    for(let i in food) {
+      if(Math.sqrt((food[i].x - x) ** 2 + (food[i].y - y) ** 2) < radius + foodRadius) {
+        socket.emit("eaten", i);
+        score++;
+        //console.log(score)
+        radius = Math.sqrt(score * 20);
+        speed = 50 / (score + 1) + 2.5
+      }
+      //console.log(Math.sqrt((food[i].x - x) ** 2 + (food[i].y - y) ** 2));
+      //console.log(alt[i]);
+      ctx.beginPath();
+      ctx.arc(food[i].x - (x - canvas.width / 2), food[i].y - (y - canvas.height / 2), foodRadius, 0, Math.PI * 2);
+      ctx.fillStyle = colors[food[i].color];
+      ctx.fill();
+    }
+
+    //Other Players
+    for(let i in alt) {
+      //console.log(alt[i]);
+      ctx.beginPath();
+      ctx.arc(alt[i].pos.x - (x - canvas.width / 2), alt[i].pos.y - (y - canvas.height / 2), alt[i].pos.r, 0, Math.PI * 2);
+      ctx.fillStyle = colors[alt[i].color];
+      ctx.fill();
+      ctx.font = `${Math.floor(alt[i].pos.r / 2.5)}px Arial`;
+      ctx.textAlign = "center";
+      ctx.fillStyle = "black"; 
+      ctx.fillText(alt[i].user, alt[i].pos.x - (x - canvas.width / 2), alt[i].pos.y - (y - canvas.height / 2) + Math.floor(alt[i].pos.r / 2.5) / 2.5);
+      
+      if(Math.sqrt((alt[i].pos.x - x) ** 2 + (alt[i].pos.y - y) ** 2) < radius - alt[i].pos.r * 0.2 && radius > alt[i].pos.r && !alt[i].pos.immune) {
+        console.log("Touch", i, alt);
+        let idd = alt[i].id;
+        score += alt[i].pos.score;
+        alt.splice(i, 1);
+        //console.log(alt);
+        //console.log(score)
+        radius = Math.sqrt(score * 20);
+        speed = 50 / (score + 1) + 2.5
+        socket.emit("kill", idd);
+      }
+    }
+
+    //Player
+    ctx.beginPath();
+    ctx.arc(canvas.width / 2 + decalX, canvas.height / 2 + decalY, radius, 0, Math.PI * 2);
+    ctx.fillStyle = colors[color];
     ctx.fill();
-    ctx.font = `${Math.floor(alt[i].pos.r / 2.5)}px Arial`;
+    ctx.font = `${Math.floor(radius / 2.5)}px Arial`;
     ctx.textAlign = "center";
     ctx.fillStyle = "black";
-    ctx.fillText(alt[i].user, alt[i].pos.x - (x - canvas.width / 2), alt[i].pos.y - (y - canvas.height / 2) + Math.floor(alt[i].pos.r / 2.5) / 2.5);
+    ctx.fillText(username, canvas.width / 2 + decalX, canvas.height / 2 + decalY + Math.floor(radius / 2.5) / 2.5);
+    ctx.stroke();
+
+    socket.emit("position", {
+      x: x + decalX,
+      y: y + decalY,
+      r: radius,
+      immune: immune,
+      score: score
+    })
+  
+    window.requestAnimationFrame(frame);
   }
+}
 
-  
-  
-  ctx.beginPath();
-  ctx.arc(canvas.width / 2 + decalX, canvas.height / 2 + decalY, radius, 0, Math.PI * 2);
-  ctx.fillStyle = colors[color];
-  ctx.fill();
-  ctx.font = `${Math.floor(radius / 2.5)}px Arial`;
-  ctx.textAlign = "center";
-  ctx.fillStyle = "black";
-  ctx.fillText(username, canvas.width / 2 + decalX, canvas.height / 2 + decalY + Math.floor(radius / 2.5) / 2.5);
-  ctx.stroke();
+const reset = () => {
+  //Movement
+  direction = {x: 0, y: 0};
+  deadzone = 1;
+  decalRange = 20;
+  decalX = 0;
+  decalY = 0;
+
+  //Players
+  x = 200;
+  y = 200;
+  radius = 20;
+  velX = 0;
+  velY = 0;
+  alt = [];
+  //let arrRight = false, arrLeft = false, arrUp = false, arrDown = false;
+  username;
+  color;
+  score = 20;
+  speed = 50 / (score + 1) + 2.5;
+  dead = false
+  immune = true;
+
+  //Map
+  spacing = 50;
+  mapWidth = 2000;
+  mapHeight = 1000;
+  borderThickness = 100000;
+
+  //Food
+  food;
+  foodRadius = 10;
 
 
-  
+  dead = false
 
-  socket.emit("position", {
-    x: x,
-    y: y,
-    r: radius
-  })
-
-  window.requestAnimationFrame(frame);
+  socket.emit("noname");
+  setTimeout(() => {
+    immune = false;
+  }, 2000);
 }
 
 
 
-document.addEventListener("keydown", e => {
+/*document.addEventListener("keydown", e => {
 
   if(e.keyCode === 37) {
     arrLeft = true;
@@ -173,7 +267,9 @@ document.addEventListener("keyup", e => {
   if(e.keyCode === 40) {
     arrDown = false;
   }
-});
+});*/
+
+//-------------------- Event listeners
 
 document.onmousemove = e => {
   direction.x = e.clientX - canvas.width / 2;
@@ -199,8 +295,9 @@ login.addEventListener("submit", e => {
       username = "Fuck you esti";
     color = Math.floor(Math.random() * 12)
     socket.emit("set name", {user: username, color: color});
-    user.value = "";
+    //user.value = "";
     loginPage.style.display = "none";
+    reset();
     frame();
   }
 });
@@ -217,6 +314,16 @@ socket.on("position", pos => {
     alt.push(pos);
   }
   //console.log(alt)
+});
+
+socket.on("food", foodArr => {
+  //console.log(food);
+  food = foodArr
+});
+
+socket.on("kill", id => {
+  dead = true
+  loginPage.style.display = "block";
 });
 
 /*socket.on("connected", pos => {
